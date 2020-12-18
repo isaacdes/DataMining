6e58743bc82180872b5f6787273a7a7d39e29404
isaacdes <de.xavier@mca.christuniversity.in>
2020-12-19 01:12:58 +0530

    My first commit for data mining

---
+++ Bins.py
@@ -0,0 +1,12 @@
+import random
+
+ranList = random.sample(range(0, 100), 100)
+# print(len(ranList))
+print(ranList)
+
+# Smoothinng by Bin means
+bins = input(print("Enter total number of bins"))
+ranList.sort()
+
+
+
